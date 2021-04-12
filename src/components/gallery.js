d387a796649c5c7e87cc548b9828f646ac6b509d
iii <miselrkba@gmail.com>
2021-04-13 00:45:39 +0200

    feat: added gallery grid images

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import galleryImage1 from '../assets/images/image01.jpg';
+import galleryImage2 from '../assets/images/image02.jpg';
+import galleryImage3 from '../assets/images/image03.jpg';
+import galleryImage4 from '../assets/images/image04.jpg';
 
 const Gallery = () => {
   return (
-    <div>
-      <h1>Gallery</h1>
+    <div id="gallery">
+      <img className="gallery__image1" src={galleryImage1} alt="deal" />
+      <img className="gallery__image2" src={galleryImage2} alt="deal" />
+      <img className="gallery__image3" src={galleryImage3} alt="deal" />
+      <img className="gallery__image4" src={galleryImage4} alt="deal" />
     </div>
   );
 };
