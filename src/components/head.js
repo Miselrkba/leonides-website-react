71c20eeaefb4494fe231a0389326203821eb1b54
iii <miselrkba@gmail.com>
2021-04-13 11:32:40 +0200

    feat: added head section

--- src/components/head.js
+++ src/components/head.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import headImg from '../assets/images/svg/head.svg';
+import circleImg from '../assets/images/svg/bg-needs-circles.svg';
 
 const Head = () => {
   return (
     <div id="head">
-      <img src={headImg} alt="head" />
-      <h1>LEONIDES</h1>
-      <p>Transform your life !</p>
+      <div className="head__container">
+        <img className="head__logo__img" src={headImg} alt="head" />
+        <img
+          src={circleImg}
+          alt="circle-needs"
+          className="head__circles__img"
+        />
+        <div className="head__title__container">
+          <h1>LEONIDES</h1>
+          <p>Transform your life !</p>
+        </div>
+      </div>
     </div>
   );
 };
