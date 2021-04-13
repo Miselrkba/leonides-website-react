712ce1c3adb05a76ec9266e88a7eee2958d72c93
iii <miselrkba@gmail.com>
2021-04-13 13:10:11 +0200

    feat: added full main page

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import menuImg from '../assets/images/svg/menu.svg';
 import ornamentImg from '../assets/images/svg/ornament.svg';
+import menuneedImg1 from '../assets/images/menu-need-1.jpg';
+import menuneedImg2 from '../assets/images/menu-need-2.jpg';
+import menuneedImg3 from '../assets/images/menu-need-3.jpg';
+import menuneedImg4 from '../assets/images/menu-need-4.jpg';
 
 const Main = () => {
   return (
@@ -10,6 +14,89 @@ const Main = () => {
         <h1 className="main__heading__title">PORTEBY ĽUDÍ</h1>
         <p>LEONIDES</p>
         <img src={ornamentImg} alt="ornament" />
+      </div>
+      <div className="main__grid">
+        <div className="main__grid__item1">
+          <h3>DETI</h3>
+        </div>
+        <div className="main__grid__item2">
+          <h3>BÝVANIE</h3>
+        </div>
+        <div className="main__grid__item3">
+          <h3>SPORENIE</h3>
+        </div>
+        <div className="main__grid__item4">
+          <h3>DÔCHODOK</h3>
+        </div>
+        <div className="main__grid__item5">
+          <img src={menuneedImg1} alt="children-whisper" />
+        </div>
+        <div className="main__grid__item6">
+          <img src={menuneedImg2} alt="villar" />
+        </div>
+        <div className="main__grid__item7">
+          <img src={menuneedImg3} alt="stacked-money" />
+        </div>
+        <div className="main__grid__item8">
+          <img src={menuneedImg4} alt="odler-couple" />
+        </div>
+      </div>
+      <div className="main__text__container">
+        <section className="main__article">
+          <article className="gold--color italic--font">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut
+            natus necessitatibus impedit aliquam mollitia cumque facere placeat
+            deleniti voluptatibus. Fugit laborum dicta ex quo quas nobis sed
+            dignissimos fugiat minus facilis libero magni officiis itaque, vitae
+            totam rem, tenetur id non repellat dolore? Illum officiis
+            exercitationem et ullam? Sunt? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Quisquam ut natus necessitatibus
+            impedit aliquam mollitia cumque facere placeat deleniti
+            voluptatibus. Fugit laborum dicta ex quo quas nobis sed dignissimos
+            fugiat minus facilis libero magni officiis itaque, vitae totam rem,
+            tenetur id non repellat dolore? Illum officiis exercitationem et
+            ullam? Sunt?
+          </article>
+          <h3>I. DETI</h3>
+          <article>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut
+            natus necessitatibus impedit aliquam mollitia cumque facere placeat
+            deleniti voluptatibus. Fugit laborum dicta ex quo quas nobis sed
+            dignissimos fugiat minus facilis libero magni officiis itaque, vitae
+            totam rem, tenetur id non repellat dolore? Illum officiis
+            exercitationem et ullam? Sunt?
+          </article>
+          <h3>II. BÝVANIE</h3>
+          <article>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            assumenda laboriosam recusandae enim voluptatum? Officiis expedita
+            dolores neque molestiae saepe ipsum nam, perspiciatis necessitatibus
+            dolor, veniam eligendi cumque a vitae ratione dicta ex nisi suscipit
+            unde minima culpa inventore aspernatur, dolore laborum
+            reprehenderit. Vel mollitia placeat rem eveniet dicta possimus
+            tempore delectus esse, tempora quibusdam nihil reiciendis inventore,
+            perspiciatis, nemo magnam ratione ipsum? Voluptatum est tenetur
+            deserunt eveniet reprehenderit neque.
+          </article>
+          <h3>III. SPORENIE</h3>
+          <article>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
+            cupiditate autem itaque dolor ut obcaecati ab maxime, hic, adipisci
+            esse, sit ipsum repudiandae? Debitis placeat eveniet provident
+            libero possimus ratione veniam soluta. Dolor perspiciatis, corporis
+            neque tempora illo ea aspernatur non animi doloribus atque, nobis
+            iste. Cumque quam nobis optio.
+          </article>
+          <h3>IV. DÔCHODOK</h3>
+          <article>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
+            cupiditate autem itaque dolor ut obcaecati ab maxime, hic, adipisci
+            esse, sit ipsum repudiandae? Debitis placeat eveniet provident
+            libero possimus ratione veniam soluta. Dolor perspiciatis, corporis
+            neque tempora illo ea aspernatur non animi doloribus atque, nobis
+            iste. Cumque quam nobis optio.
+          </article>
+        </section>
       </div>
     </div>
   );
