fd4f9030f943e7aa4df096421f87907593960b72
iii <miselrkba@gmail.com>
2021-04-14 10:07:04 +0200

    fix: fixed form validation

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const ContactForm = () => (
   <div id="contactForm">
     <Formik
       initialValues={{
-        meno: 'MENO',
-        email: 'EMAIL',
-        telefon: 'TELEFÓN',
-        odkaz: 'ODKAZ...',
+        meno: '',
+        email: '',
+        telefon: '',
+        odkaz: '',
       }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
+      validationSchema={Yup.object({
+        meno: Yup.string().required('Toto pole musí byť vyplnené'),
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Toto pole musí byť vyplnené'),
+        telefon: Yup.number()
+          .required('Toto pole musí byť vyplnené')
+          .positive(),
+        odkaz: Yup.string()
+          .min(10, 'Odkaz musí mať minimálne 10 znakov')
+          .required('Toto pole musí byť vyplnené'),
+      })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           // eslint-disable-next-line no-alert
@@ -32,17 +34,34 @@ const ContactForm = () => (
       {({ isSubmitting }) => (
         <Form className="contactForm__container">
           <div className="contactForm__container__main">
-            <Field type="name" name="meno" />
-            <ErrorMessage name="meno" component="div" />
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="tel" name="telefon" />
-            <ErrorMessage name="telefon" component="div" />
+            <Field type="name" name="meno" placeholder="MENO" />
+            <ErrorMessage
+              name="meno"
+              component="div"
+              style={{ color: 'red' }}
+            />
+            <Field type="email" name="email" placeholder="EMAIL" />
+            <ErrorMessage
+              name="email"
+              component="div"
+              style={{ color: 'red' }}
+            />
+            <Field type="tel" name="telefon" placeholder="TELEFÓN" />
+            <ErrorMessage
+              name="telefon"
+              component="div"
+              style={{ color: 'red' }}
+            />
           </div>
           <div className="contactForm__container__textarea">
-            <Field type="textarea" name="odkaz" />
+            <Field
+              name="odkaz"
+              as="textarea"
+              className="contactForm__textarea"
+              placeholder="ODKAZ"
+            />
           </div>
-          <ErrorMessage name="odkaz" component="div" />
+          <ErrorMessage name="odkaz" component="div" style={{ color: 'red' }} />
           <div className="contactForm__container__button">
             <button type="submit" disabled={isSubmitting}>
               ODOSLAŤ
