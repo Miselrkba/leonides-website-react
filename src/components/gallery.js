3fca4bdcdd61f57841292882e9d0f5be877ab5a3
iii <miselrkba@gmail.com>
2021-04-21 10:21:08 +0200

    feat: added gallery transparent backgroud

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/mouse-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import galleryImage1 from '../assets/images/image01.jpg';
 import galleryImage2 from '../assets/images/image02.jpg';
@@ -7,10 +10,21 @@ import galleryImage4 from '../assets/images/image04.jpg';
 const Gallery = () => {
   return (
     <div id="gallery">
-      <img className="gallery__image1" src={galleryImage1} alt="deal" />
-      <img className="gallery__image2" src={galleryImage2} alt="deal" />
-      <img className="gallery__image3" src={galleryImage3} alt="deal" />
-      <img className="gallery__image4" src={galleryImage4} alt="deal" />
+      <div className="gallery__image1">
+        <div id="book1">
+          <h3>ZMEŇ SVOJ ŽIVOT</h3>
+        </div>
+        <img src={galleryImage1} alt="deal" />
+      </div>
+      <div className="gallery__image2">
+        <img src={galleryImage2} alt="man-poiting-finger" />
+      </div>
+      <div className="gallery__image3">
+        <img src={galleryImage3} alt="discusion" />
+      </div>
+      <div className="gallery__image4">
+        <img src={galleryImage4} alt="smiling-guy" />
+      </div>
     </div>
   );
 };
