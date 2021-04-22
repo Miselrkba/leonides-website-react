4d4625d1464bcfac3ceea05654e4c5a60cf978fd
iii <miselrkba@gmail.com>
2021-04-22 09:59:10 +0200

    fix: moved more components to lazy load

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,32 @@
 import React, { lazy, Suspense } from 'react';
-import About from './about';
-import Contact from './contact';
-import Gallery from './gallery';
-import Head from './head';
+import Navbar from './navbar';
 import Hero from './hero';
 import Logo from './logo';
-import Main from './main';
-import Navbar from './navbar';
-import Footer from './footer';
+import About from './about';
 import Fallback from './fallback';
 
+const Gallery = lazy(() => import('./gallery'));
+const Head = lazy(() => import('./head'));
+const Main = lazy(() => import('./main'));
 const Maps = lazy(() => import('./maps'));
+const Contact = lazy(() => import('./contact'));
+const Footer = lazy(() => import('./footer'));
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Logo />
+      <About />
       <Suspense fallback={<Fallback />}>
-        <Logo />
-        <About />
         <Gallery />
         <Head />
         <Main />
         <Maps />
+        <Contact />
+        <Footer />
       </Suspense>
-      <Contact />
-      <Footer />
     </>
   );
 };
