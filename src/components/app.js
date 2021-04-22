2c9d8f17b093dd61a402096b99eb63b87d0f6fcd
iii <miselrkba@gmail.com>
2021-04-22 17:22:07 +0200

    test: testing fallback options

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,24 @@
 import React, { lazy, Suspense } from 'react';
+// import Fallback from './fallback';
 import Navbar from './navbar';
 import Hero from './hero';
 import Logo from './logo';
 import About from './about';
+import Maps from './maps';
+import Contact from './contact';
+// import Gallery from './gallery';
+// import Head from './head';
 import Fallback from './fallback';
+// import Main from './main';
+import Footer from './footer';
 
 // lazy loaded components to improve page load
 const Gallery = lazy(() => import('./gallery'));
 const Head = lazy(() => import('./head'));
 const Main = lazy(() => import('./main'));
-const Maps = lazy(() => import('./maps'));
-const Contact = lazy(() => import('./contact'));
-const Footer = lazy(() => import('./footer'));
+// const Maps = lazy(() => import('./maps'));
+// const Contact = lazy(() => import('./contact'));
+// const Footer = lazy(() => import('./footer'));
 
 // suspense is loading component to show loading sign while page is loaded
 const App = () => {
@@ -25,10 +32,10 @@ const App = () => {
         <Gallery />
         <Head />
         <Main />
-        <Maps />
-        <Contact />
-        <Footer />
       </Suspense>
+      <Maps />
+      <Contact />
+      <Footer />
     </>
   );
 };
