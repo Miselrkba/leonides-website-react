86d84fd489b38e82f37249c1fc9af59b20aad25f
iii <miselrkba@gmail.com>
2021-04-25 09:07:07 +0200

    fix: updated comments

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,19 @@
 import React, { lazy, Suspense } from 'react';
-// import Fallback from './fallback';
 import Navbar from './navbar';
 import Hero from './hero';
 import Logo from './logo';
 import About from './about';
 import Maps from './maps';
 import Contact from './contact';
-// import Gallery from './gallery';
-// import Head from './head';
 import Fallback from './fallback';
-// import Main from './main';
 import Footer from './footer';
 
-// lazy loaded components to improve page load
+// lazy load image heavy components
 const Gallery = lazy(() => import('./gallery'));
 const Head = lazy(() => import('./head'));
 const Main = lazy(() => import('./main'));
-// const Maps = lazy(() => import('./maps'));
-// const Contact = lazy(() => import('./contact'));
-// const Footer = lazy(() => import('./footer'));
 
-// suspense is loading component to show loading sign while page is loaded
+// suspense to show fallback loading... component
 const App = () => {
   return (
     <>
